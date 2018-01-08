114f5e3a3d400cd414add06f275e9c16dd2fe6d6
Aurélien Rodot <rodot@gamebuino.com>
2018-01-08 18:57:21 +0100

    Move camera

--- EngineMETA/Engine.cpp
+++ EngineMETA/Engine.cpp
@@ -17,8 +17,10 @@ void Engine::init() {
 
 void Engine::update() {
   for (int i = 0; i < ENGINE_NUM_OBJECTS; i++) {
+    if (objects[i] == 0) continue;
     objects[i]->update();
     for (int j = 0; j < ENGINE_NUM_OBJECTS; j++) {
+      if (objects[j] == 0) continue;
       if (i == j) {
         continue;
       }
@@ -34,9 +36,8 @@ void Engine::draw() {
     map->draw();
   }
   for (int i = 0; i < ENGINE_NUM_OBJECTS; i++) {
-    if (objects[i] != 0) {
-      objects[i]->draw();
-    }
+    if (objects[i] == 0) continue;
+    objects[i]->draw();
   }
   gb.display.print(gb.getCpuLoad());
   gb.display.print("%");
--- EngineMETA/EngineMETA.ino
+++ EngineMETA/EngineMETA.ino
@@ -6,7 +6,10 @@ void setup() {
   gb.begin();
   engine.init();
 
-  while (engine.addObject(new Object()));
+  //while (engine.addObject(new Object()));
+  engine.addObject(new Object());
+  engine.addObject(new Object());
+  engine.addObject(new Object());
 
   engine.setTileMap(new TileMap());
 }
@@ -30,15 +33,30 @@ void loop() {
   if (gb.buttons.pressed(BUTTON_A)) {
     Engine::gravity *= -1;
   }
+  Engine::cameraX = Engine::objects[0]->x - gb.display.width() / 2;
+  Engine::cameraY = Engine::objects[0]->y - gb.display.height() / 2;
   if (gb.buttons.pressed(BUTTON_B)) {
-    for (uint16_t i = 0; i < ENGINE_NUM_OBJECTS; i++) {
+    /*for (uint16_t i = 0; i < ENGINE_NUM_OBJECTS; i++) {
       Engine::objects[i]->x = random(8, 72);
       Engine::objects[i]->y = random(8, 56);
       Engine::objects[i]->width = random(4,8);
       Engine::objects[i]->height = random(4,8);
       Engine::objects[i]->vx = random(0, 3) - 1;
       Engine::objects[i]->vy = random(0, 3) - 1;
-    }
+      }*/
+    Engine::objects[0]->x = random(8, 72);
+    Engine::objects[0]->y = random(8, 56);
+    Engine::objects[0]->width = random(4, 8);
+    Engine::objects[0]->height = random(4, 8);
+    Engine::objects[0]->vx = random(0, 3) - 1;
+    Engine::objects[0]->vy = random(0, 3) - 1;
+
+    Engine::objects[1]->width = random(4, 20);
+    Engine::objects[1]->height = random(4, 20);
+    Engine::objects[1]->x = 35;
+    Engine::objects[1]->y = 25;
+    Engine::objects[1]->vx = 0;
+    Engine::objects[1]->vy = 0;
   }
   engine.update();
   engine.draw();
